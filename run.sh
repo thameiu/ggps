32f3c38e79017300cdf56113970899fbf99aa4d8
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2024-10-15 09:52:43 +0200

    modified run.sh so it has arg options and launches the backend docker and does npm run dev instead

--- run.sh
+++ run.sh
@@ -1,2 +1,47 @@
-cd ./frontend/app
-docker compose up -d
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <build|down>"
+  exit 1
+fi
+
+# If the argument is 'build', perform the following actions
+if [ "$1" == "build" ]; then
+  sudo chmod -R 777 .
+
+  cd ./backend/ggps-backend || { echo "Directory ./backend/ggps-backend does not exist."; exit 1; }
+
+  docker compose up --build -d
+
+  docker compose exec app php artisan migrate
+
+  cd ../../frontend/app || { echo "Directory ../../frontend/app does not exist."; exit 1; }
+
+  npm run dev
+
+
+elif [ "$1" == "start" ]; then
+  sudo chmod -R 777 .
+
+  cd ./backend/ggps-backend || { echo "Directory ./backend/ggps-backend does not exist."; exit 1; }
+
+  docker compose up -d
+
+  cd ../../frontend/app || { echo "Directory ../../frontend/app does not exist."; exit 1; }
+
+  npm run dev
+
+
+elif [ "$1" == "down" ]; then
+  cd ./backend/ggps-backend || { echo "Directory ./backend/ggps-backend does not exist."; exit 1; }
+
+  docker compose down
+
+  echo "Docker containers stopped."
+  
+
+else
+  echo "Invalid argument. Use 'build' to build and start, or 'down' to stop the containers."
+  exit 1
+fi
